a485d5902d4a9029cda4c3846d2876199d1bcd34
Tim Mays <tim@tim-mays.com>
2019-11-10 13:20:52 -0500

    adds company and contact data to the main views

--- client/src/views/companies.tsx
+++ client/src/views/companies.tsx
@@ -1,12 +1,83 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-type Props = {};
-type State = {};
+interface Company {
+  companyId: number;
+  companyName: string;
+  websiteURI: string;
+  phoneNumber: string;
+  streetAddress: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+interface CompanyData {
+  getCompanies: Company[];
+}
+
+const COMPANIES_QUERY = gql`
+  query companies {
+    getCompanies {
+      companyId
+      companyName
+      websiteURI
+      phoneNumber
+      streetAddress
+      city
+      state
+      zipCode
+    }
+  }
+`;
 
 const Companies: React.FC = () => {
+  const { loading, data } = useQuery<CompanyData>(COMPANIES_QUERY);
   return (
     <div className="home">
       <h2>Aquent Developer Candidate Project - Companies</h2>
+      {loading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Company Name</th>
+              <th>Website</th>
+              <th>Phone Number</th>
+              <th>Street Address</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.getCompanies.map(company => (
+                <tr key={company.companyId}>
+                  <td>{company.companyId}</td>
+                  <td>{company.companyName}</td>
+                  <td>
+                    <a
+                      href={company.websiteURI}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {company.websiteURI}
+                    </a>
+                  </td>
+                  <td>{company.phoneNumber}</td>
+                  <td>{company.streetAddress}</td>
+                  <td>{company.city}</td>
+                  <td>{company.state}</td>
+                  <td>{company.zipCode}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
--- client/src/views/contacts.tsx
+++ client/src/views/contacts.tsx
@@ -1,9 +1,83 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+interface Person {
+  personId: number;
+  companyId: number;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  streetAddress: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+interface PersonData {
+  getPeople: Person[];
+}
+
+const PEOPLE_QUERY = gql`
+  query people {
+    getPeople {
+      personId
+      companyId
+      firstName
+      lastName
+      emailAddress
+      streetAddress
+      city
+      state
+      zipCode
+    }
+  }
+`;
 
 const Contacts: React.FC = () => {
+  const { loading, data } = useQuery<PersonData>(PEOPLE_QUERY);
   return (
     <div className="home">
       <h2>Aquent Developer Candidate Project - Contacts</h2>
+      {loading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Company ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email Address</th>
+              <th>Street Address</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.getPeople.map(person => (
+                <tr key={person.personId}>
+                  <td>{person.personId}</td>
+                  <td>{person.companyId}</td>
+                  <td>{person.firstName}</td>
+                  <td>{person.lastName}</td>
+                  <td>
+                    <a href={`mailto:${person.emailAddress}`}>
+                      {person.emailAddress}
+                    </a>
+                  </td>
+                  <td>{person.streetAddress}</td>
+                  <td>{person.city}</td>
+                  <td>{person.state}</td>
+                  <td>{person.zipCode}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
