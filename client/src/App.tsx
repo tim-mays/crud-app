1e1c656500461612d83cffc38e1d051551e808ba
Tim Mays <tim@tim-mays.com>
2019-11-10 11:18:00 -0500

    adds Apollo client for GraphQL support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Routes from './routes';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:8081/graphql' // TODO: move this to env
+});
 
 const App: React.FC = () => {
   return (
-    <React.Fragment>
-      <Routes />
-    </React.Fragment>
+    <ApolloProvider client={apolloClient}>
+      <React.Fragment>
+        <Routes />
+      </React.Fragment>
+    </ApolloProvider>
   );
 };
 
